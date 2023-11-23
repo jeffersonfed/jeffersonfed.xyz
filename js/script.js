f7f4926fed67aab22c4e388f85f7547baaccc18b
Jefferson <stupidjeff430@gmail.com>
2023-11-23 22:29:31 +0900

    add randomtext display

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ function toggleTheme() {
     else
         document.body.classList.add("dark");
 }
+
+const words = ["FED", "RJ14","FED", "RJ14","FED", "RJ14","FED", "RJ14",];
+
+!function newWord() {
+  const randomWord = Math.floor(Math.random() * words.length);
+  document.getElementById('wordDisplay').innerHTML = words[randomWord];
+}();

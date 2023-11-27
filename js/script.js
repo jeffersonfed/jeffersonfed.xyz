5ed87f7692aedbd64623b7c1cc57d7223697e710
Jefferson <stupidjeff430@gmail.com>
2023-11-27 13:27:35 +0900

    #wordDisplay Javascript Change

--- js/script.js
+++ js/script.js
@@ -5,9 +5,26 @@ function toggleTheme() {
         document.body.classList.add("dark");
 }
 
-const words = ["FED", "RJ14","FED", "RJ14","FED", "RJ14","FED", "RJ14",];
+// const words = ["FED", "RJ14","FED", "RJ14","FED", "RJ14","FED", "RJ14",];
 
-!function newWord() {
-  const randomWord = Math.floor(Math.random() * words.length);
-  document.getElementById('wordDisplay').innerHTML = words[randomWord];
-}();
+// (!)function newWord() {
+//   const randomWord = Math.floor(Math.random() * words.length);
+//   document.getElementById('wordDisplay').innerHTML = words[randomWord];
+// }();
+
+(function(){
+  var words = [
+      ' FED',
+      ' RJ14',
+      ' FED',
+      ' RJ14',
+      ' FED',
+      ' RJ14'
+      ], i = 0;
+  setInterval(function(){
+      $('#wordDisplay').fadeOut(function(){
+          $(this).html(words[i=(i+1)%words.length]).fadeIn();
+      });
+  }, 2000);
+    
+})();
